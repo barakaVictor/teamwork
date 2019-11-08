993d3394ab514e9309b694606028a0f8de0141ba
barakaVictor <barakavictor62@gmail.com>
2019-11-09 02:17:52 +0300

    Initial boilerplate configuration of the project

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const bodyParser = require('body-parser');
+
+const routes = require('./routes');
+
+app.use(bodyParser.json());
+
+app.use('/api/v1', routes);
+
+module.exports = app;
---
+++ test/hello.spec.js
@@ -0,0 +1,7 @@
+const assert = require('assert');
+
+describe('Hello', () => {
+  it('Renders default message', () => {
+    assert.equal(1, 1);
+  });
+});
