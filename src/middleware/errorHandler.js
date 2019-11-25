81960b2f4e322e78afc0481eada010f9da5b8788
barakaVictor <barakavictor62@gmail.com>
2019-11-25 11:33:02 +0300

    Revert "created directory structure for api routes versioning"
    This reverts commit that created api versioning directory structure.

--- src/middleware/errorHandler.js
+++
@@ -1,9 +0,0 @@
-module.exports = (error, request, response, next) => {
-  console.error(error);
-  return response
-    .status(500)
-    .json({
-    	status: 'error',
-    	error: 'Ooops!! Something went wrong.',
-    });
-};
