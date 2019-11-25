a76d38f4f36d883f8e6cf55e80a23f3ead5dc09e
barakaVictor <barakavictor62@gmail.com>
2019-11-25 14:23:06 +0300

    added .env file specified node version in package.json

--- src/db/index.js
+++ src/db/index.js
@@ -1,5 +1,19 @@
-const pgp = require('pg-promise')();
+const initOptions = {
+  query(e) {
+    console.log('QUERY:', e.query);
+  },
+};
 
-const db = pgp('postgres://username:password@host:port/database');
+const cn = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+};
+
+const pgp = require('pg-promise')(initOptions);
+
+const db = pgp(cn);
 
 module.exports = db;
