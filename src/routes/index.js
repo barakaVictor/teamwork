e7d40d48349504195012bcd67fe5d07f9dd1cda4
barakaVictor <barakavictor62@gmail.com>
2019-11-28 12:05:18 +0300

    feature(UserController.createUser):add createUser method in Usercontroller class
    add create-user route in routes module
    add sinon as project dependency for creating mocks, stubs, spies
    add httpmocks module in tests/testutils containing http requests,
    response and next mocks
    add unit test for createUser method of the UserController class in
    tests/unit
    update test script in package.json to target files in test/unit
    
    [Start]

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,16 @@ const express = require('express');
 
 const router = express.Router();
 
+const UserController = require('../controllers/usercontroller');
+
+const userController = new UserController();
+
 router.get('/', (request, response) => {
   response.status(200).json({
     message: 'Hello from teamwork!!',
   });
 });
+
+router.post('/create-user', userController.createUser);
 
 module.exports = router;
---
+++ test/unit/createuser.spec.js
@@ -0,0 +1,41 @@
+const assert = require('assert');
+
+const { mockRequest, mockResponse, mockNext } = require('../testutils/httpmocks');
+
+const UserController = require('../../src/controllers/usercontroller');
+
+describe('UserController.createUser', () => {
+  it('Returns 201 success response code on successful user creation', (done) => {
+    function UserModel() {
+      return {
+        async createUser() {
+          return Promise.resolve(true);
+        },
+      };
+    }
+
+    const request = mockRequest({
+      body: {
+        firstName: 'test',
+        lastName: 'test',
+        email: 'test@example.com',
+        password: 'test',
+        gender: 'male',
+        jobRole: 'Developer',
+        department: 'IT',
+        address: '00018-Downing Street',
+      },
+    });
+
+    const response = mockResponse();
+    const next = mockNext();
+    const userController = new UserController(UserModel);
+
+    userController.createUser(request, response, next)
+      .then((resp) => {
+        assert.equal(resp.status.args[0][0], 201);
+        done();
+      })
+      .catch((error) => done(error));
+  });
+});
