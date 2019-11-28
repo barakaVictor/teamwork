c2952627d1d2188d0e5110e8ec464f2a8027e688
barakaVictor <barakavictor62@gmail.com>
2019-11-28 14:11:32 +0300

    feature(UserModel) add UserModel for db access
    add BaseModel class to encapsulate basic CRUD actions
    add UserModel to inherit from BaseClass and override methods as required
    inject UserModel class to UserController instantiation in route definitions to allow controller to communicate with the database

---
+++ src/models/base.js
@@ -0,0 +1,68 @@
+class BaseModel {
+  constructor(db, table) {
+    this.db = db;
+    this.table = table;
+    this.all = this.all.bind(this);
+    this.find = this.find.bind(this);
+    this.save = this.save.bind(this);
+    this.delete = this.delete.bind(this);
+    this.truncate = this.truncate.bind(this);
+  }
+
+  async find(query) {
+    return this.db.any(
+      'SELECT * FROM $1:name WHERE $2:name = $2:list',
+      [this.table, query],
+    )
+      .then((obj) => obj)
+      .catch((error) => {
+        throw new Error(error);
+      });
+  }
+
+  async all() {
+    return this.db.any(
+      'SELECT $1:name FROM $2:name',
+      ['*', this.table],
+    )
+      .then((result) => result)
+      .catch((error) => {
+        throw new Error(error);
+      });
+  }
+
+  async save(obj) {
+    return this.db.none(
+      'INSERT INTO $1:name($2:name) VALUES($2:list)',
+      [this.table, obj],
+    )
+      .then(() => true)
+      .catch((error) => {
+        throw new Error(error);
+      });
+  }
+
+  async delete(args) {
+    return this.db.none(
+      'DELETE FROM $1:name WHERE $2:name = $2:list',
+      [this.table, args],
+    )
+      .then(() => true)
+      .catch((error) => {
+        throw new Error(error);
+      });
+  }
+
+  async truncate() {
+    return this.db.none(
+      'TRUNCATE TABLE $1:name RESTART IDENTITY',
+      [this.table],
+    )
+      .then(() => true)
+      .catch((error) => {
+        throw new Error(error);
+      });
+  }
+}
+
+module.exports = BaseModel;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 
 const router = express.Router();
-
+const UserModel = require('../models/user');
 const UserController = require('../controllers/usercontroller');
 
-const userController = new UserController();
+const userController = new UserController(UserModel);
 
 router.get('/', (request, response) => {
   response.status(200).json({
