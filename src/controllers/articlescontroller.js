235bbc90e77feb77924e4713b584b29cc8a84c87
barakaVictor <barakavictor62@gmail.com>
2019-12-07 21:02:02 +0300

    feature(patch article): add functionality to update articles
    add test suite to test patch method to be added to ArticlesController
    add patch method to ArticlesController
    
    [Start]

--- src/controllers/articlescontroller.js
+++ src/controllers/articlescontroller.js
@@ -16,6 +16,30 @@ class ArticlesController {
         },
       })).catch((error) => next(error));
   }
+
+  async patch(request, response, next) {
+    return this.model.find({ id: request.params.articleId })
+      .then((article) => {
+        if (!article) {
+          return response.status(404).json({
+            status: 'error',
+            error: 'The requested was not found!!',
+          });
+        }
+        const newArticle = request.body;
+        return this.model.update(newArticle)
+          .then((savedArticle) => response.status(200).json({
+            status: 'Success',
+            data: {
+              message: 'Article successfully updated',
+              title: savedArticle.title,
+              article: savedArticle.article,
+            },
+          })).catch((error) => {
+            throw new Error(error);
+          });
+      }).catch((error) => next(error));
+  }
 }
 
 module.exports = ArticlesController;
---
+++ test/unit/patchpost.spec.js
@@ -0,0 +1,77 @@
+const assert = require('assert');
+
+const { mockRequest, mockResponse, mockNext } = require('../testutils/httpmocks');
+
+const ArticlesController = require('../../src/controllers/articlescontroller');
+
+describe('ArticlesController.patch', () => {
+  let request;
+  let response;
+  let next;
+  let articlesModel;
+  let articlesController;
+
+  beforeEach(() => {
+    request = mockRequest({
+      params: {
+        articleId: 1,
+      },
+      body: {
+        title: 'test',
+        article: 'new content coming through',
+      },
+    });
+    response = mockResponse();
+    next = mockNext();
+  });
+  it('Returns 200 Ok message on successful article edit function', (done) => {
+    articlesModel = function ArticleModel() {
+      return {
+        update: (article) => Promise.resolve(article),
+        find: () => Promise.resolve({}),
+      };
+    };
+    articlesController = new ArticlesController(articlesModel);
+    articlesController.patch(request, response, next)
+      .then((resp) => {
+        assert.equal(resp.status.args[0][0], 200);
+        done();
+      })
+      .catch((error) => done(error));
+  });
+
+  it('Returns 404 error code if article to be edited is non-existent', (done) => {
+    articlesModel = function ArticleModel() {
+      return {
+        find: () => Promise.resolve(null),
+      };
+    };
+
+    articlesController = new ArticlesController(articlesModel);
+    articlesController.patch(request, response, next)
+      .then((resp) => {
+        assert.equal(resp.status.args[0][0], 404);
+        done();
+      })
+      .catch((error) => done(error));
+  });
+
+  it('Calls next when an error is encountered', (done) => {
+    articlesModel = function ArticleModel() {
+      return {
+        find: () => Promise.resolve({
+          title: 'test',
+          article: 'some content',
+        }),
+        update: () => Promise.reject(),
+      };
+    };
+    articlesController = new ArticlesController(articlesModel);
+    articlesController.patch(request, response, next)
+      .then(() => {
+        assert(next.called);
+        done();
+      })
+      .catch((error) => done(error));
+  });
+});
