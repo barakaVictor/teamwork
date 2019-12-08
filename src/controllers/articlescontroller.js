24456f0a3b802d92107f8cc7c878810b8e151b65
barakaVictor <barakavictor62@gmail.com>
2019-12-08 17:03:15 +0300

    feature(delete article): add delete article method to ArticlesController
    add test suite for delete method in ArticlesController
    add delete method to ArticlesController
    
    [Start]

--- src/controllers/articlescontroller.js
+++ src/controllers/articlescontroller.js
@@ -3,6 +3,7 @@ class ArticlesController {
     this.model = new Model();
     this.createArticle = this.createArticle.bind(this);
     this.patch = this.patch.bind(this);
+    this.delete = this.delete.bind(this);
   }
 
   async createArticle(request, response, next) {
@@ -37,6 +38,28 @@ class ArticlesController {
               article: savedArticle.article,
             },
           })).catch((error) => {
+            throw new Error(error);
+          });
+      }).catch((error) => next(error));
+  }
+
+  async delete(request, response, next) {
+    return this.model.find({ id: request.params.articleId })
+      .then((article) => {
+        if (!article) {
+          return response.status(404).json({
+            status: 'error',
+            error: 'The requested articles could not be found',
+          });
+        }
+        return this.model.delete(article)
+          .then(() => response.status(200).json({
+            status: 'Success',
+            data: {
+              message: 'Article successfully deleted',
+            },
+          }))
+          .catch((error) => {
             throw new Error(error);
           });
       }).catch((error) => next(error));
---
+++ test/unit/deletearticle.spec.js
@@ -0,0 +1,80 @@
+const assert = require('assert');
+
+const { mockRequest, mockResponse, mockNext } = require('../testutils/httpmocks');
+
+const ArticlesController = require('../../src/controllers/articlescontroller');
+
+describe('ArticlesController.delete', () => {
+  let articlesController;
+  let request;
+  let response;
+  let next;
+  beforeEach(() => {
+    request = mockRequest({
+      params: {
+        articleId: 1,
+      },
+    });
+    response = mockResponse();
+    next = mockNext();
+  });
+
+  it('Returns 200 Ok upon successfull article deletion', (done) => {
+    function DummyModel() {
+      return {
+        delete: () => Promise.resolve(),
+        find: () => Promise.resolve({
+          id: 1,
+          title: 'test title',
+          article: 'some content',
+          created_on: new Date(),
+        }),
+      };
+    }
+    articlesController = new ArticlesController(DummyModel);
+
+    articlesController.delete(request, response, next)
+      .then((resp) => {
+        assert.equal(resp.status.args[0][0], 200);
+        assert.equal(resp.json.args[0][0].data.message, 'Article successfully deleted');
+        done();
+      })
+      .catch((error) => done(error));
+  });
+
+  it('Returns 404 not found when requested article to delete does not exist', (done) => {
+    function DummyModel() {
+      return {
+        find: () => Promise.resolve(null),
+      };
+    }
+
+    articlesController = new ArticlesController(DummyModel);
+    articlesController.delete(request, response, next)
+      .then((resp) => {
+        assert.equal(resp.status.args[0][0], 404);
+        done();
+      })
+      .catch((error) => done(error));
+  });
+
+  it('Calls next atleast once when an erro is encoutered', (done) => {
+    function DummyModel() {
+      return {
+        delete: () => Promise.reject(),
+        find: () => Promise.resolve({
+          id: 1,
+          title: 'some title',
+          article: 'article content',
+          created_on: new Date(),
+        }),
+      };
+    }
+    articlesController = new ArticlesController(DummyModel);
+    articlesController.delete(request, response, next)
+      .then(() => {
+        assert(next.called);
+        done();
+      }).catch((error) => done(error));
+  });
+});
