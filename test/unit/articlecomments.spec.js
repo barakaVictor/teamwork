65bba5e407a9bdfa4b7831adeb43892f5b1a1d4e
barakaVictor <barakavictor62@gmail.com>
2019-12-08 22:17:08 +0300

    feature(commentOnArticle): add CommentsController to handle comments
    add test suite to test the ability to post a comment related to an
    article
    add a CommentsController module to hold logic for handling a comment
    http request
    add commentOnArticle method in CommentsController to handle comments
    intended for an article
    add commentOnGif method to CommentsController to handle comments
    intended for a gif

---
+++ test/unit/articlecomments.spec.js
@@ -0,0 +1,65 @@
+const assert = require('assert');
+
+const { mockRequest, mockResponse, mockNext } = require('../testutils/httpmocks');
+
+const CommentsController = require('../../src/controllers/commentscontroller');
+
+describe('CommentsController.commentOnArticle', () => {
+  let request;
+  let response;
+  let next;
+  let commentsController;
+
+  beforeEach(() => {
+    request = mockRequest({
+      params: {
+        articleId: 1,
+      },
+      body: {
+        comment: 'Here comes the comment',
+        authorId: 1,
+      },
+    });
+    response = mockResponse();
+    next = mockNext();
+  });
+
+  it('Returns 200 Ok http response when a comment is posted successfully', (done) => {
+    function DummyModel() {
+      return {
+        save: () => Promise.resolve({
+          article: {
+            title: 'Some title',
+            article: 'Here is some article content',
+          },
+          comment: {
+            created_on: new Date(),
+            comment: 'Some random comment',
+          },
+        }),
+      };
+    }
+    commentsController = new CommentsController(DummyModel);
+    commentsController.commentOnArticle(request, response, next)
+      .then((resp) => {
+        assert.equal(resp.status.args[0][0], 200);
+        done();
+      })
+      .catch((error) => done(error));
+  });
+
+  it('Calls next on when an error is encountered in the execution process', (done) => {
+    function DummyModel() {
+      return {
+        save: () => Promise.reject(),
+      };
+    }
+    commentsController = new CommentsController(DummyModel);
+    commentsController.commentOnArticle(request, response, next)
+      .then(() => {
+        assert(next.called);
+        done();
+      })
+      .catch((error) => done(error));
+  });
+});
