4c01c81d58fe6b63d99311262c283d21398d53dd
barakaVictor <barakavictor62@gmail.com>
2019-12-08 18:04:30 +0300

    feature(gif delete route): add delete route to gif route definitions
    [Finishes]

--- src/routes/api/v1/gifs/index.js
+++ src/routes/api/v1/gifs/index.js
@@ -7,5 +7,6 @@ const GifController = require('../../../../controllers/gifcontroller');
 const gifController = new GifController(GifModel);
 
 router.post('/', gifController.upload);
+router.delete('/:gifId', gifController.delete);
 
 module.exports = router;
