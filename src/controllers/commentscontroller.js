d51ac66dbc875d7e33725b66455457ed061089dc
barakaVictor <barakavictor62@gmail.com>
2019-12-14 08:15:16 +0300

    feature(comments table): add a comments table to the database

--- src/controllers/commentscontroller.js
+++ src/controllers/commentscontroller.js
@@ -2,13 +2,12 @@ class CommentsController {
   constructor(Model) {
     this.model = new Model();
     this.commentOnArticle = this.commentOnArticle.bind(this);
-    this.commentOnGif = this.commentOnGif.bind(this);
   }
 
   async commentOnArticle(request, response, next) {
     const comment = {
-      articleId: request.params.articleId,
-      userId: request.body.authorID,
+      articleid: request.params.articleId,
+      userid: request.body.authorID,
       comment: request.body.comment,
     };
     return this.model.save(comment)
@@ -22,13 +21,6 @@ class CommentsController {
           comment: result.comment.comment,
         },
       })).catch((error) => next(error));
-  }
-
-  async commentOnGif(request, response, next) {
-    return response.status(400).json({
-      status: 'error',
-      error: 'This functionality is yet to be implemented',
-    });
   }
 }
 
--- src/routes/api/v1/articles/index.js
+++ src/routes/api/v1/articles/index.js
@@ -1,12 +1,18 @@
 const router = require('express').Router();
 
 const ArticlesModel = require('../../../../models/article');
+const ArticleCommentsModel = require('../../../../models/articlecomments');
 const ArticlesController = require('../../../../controllers/articlescontroller');
+const CommentsController = require('../../../../controllers/commentscontroller');
+
 
 const articlesController = new ArticlesController(ArticlesModel);
+const commentsController = new CommentsController(ArticleCommentsModel);
+
 
 router.post('/', articlesController.createArticle);
 router.patch('/:articleId', articlesController.patch);
 router.delete('/:articleId', articlesController.delete);
+router.post('/:articleId/comment', commentsController.commentOnArticle);
 
 module.exports = router;
