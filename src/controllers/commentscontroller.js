eea4ead6843df745f54ec98d1ab20fbea1b74c9a
barakaVictor <barakavictor62@gmail.com>
2019-12-15 15:44:34 +0300

    feature(comment on gif): add method to save comment related to gif
    add commentOnGif method to the CommentsController that will the model to insert a comment posted on a gif to
    the database

--- src/controllers/commentscontroller.js
+++ src/controllers/commentscontroller.js
@@ -2,6 +2,7 @@ class CommentsController {
   constructor(Model) {
     this.model = new Model();
     this.commentOnArticle = this.commentOnArticle.bind(this);
+    this.commentOnGif = this.commentOnGif.bind(this);
   }
 
   async commentOnArticle(request, response, next) {
@@ -18,6 +19,24 @@ class CommentsController {
           createdOn: result.comment.created_on,
           articleTitle: result.article.title,
           article: result.article.article,
+          comment: result.comment.comment,
+        },
+      })).catch((error) => next(error));
+  }
+
+  async commentOnGif(request, response, next) {
+    const comment = {
+      gifid: request.params.gifId,
+      userid: request.body.authorID,
+      comment: request.body.comment,
+    };
+    return this.model.save(comment)
+      .then((result) => response.status(200).json({
+        status: 'Success',
+        data: {
+          message: 'Comment successfully created',
+          createdOn: result.comment.created_on,
+          gifTitle: result.gif.title,
           comment: result.comment.comment,
         },
       })).catch((error) => next(error));
