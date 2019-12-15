f84d98cc0c4c2cb97a6343d363421f6a18dc7974
barakaVictor <barakavictor62@gmail.com>
2019-12-15 15:41:24 +0300

    test(Gif comments): add test suite for Gif comments
    [Start]

---
+++ test/unit/gifcomments.js
@@ -0,0 +1,64 @@
+const assert = require('assert');
+
+const { mockRequest, mockResponse, mockNext } = require('../testutils/httpmocks');
+
+const CommentsController = require('../../src/controllers/commentscontroller');
+
+describe('CommentsController.commentOnGif', () => {
+  let request;
+  let response;
+  let next;
+  let commentsController;
+
+  beforeEach(() => {
+    request = mockRequest({
+      params: {
+        articleId: 1,
+      },
+      body: {
+        comment: 'Here comes the comment',
+        authorId: 1,
+      },
+    });
+    response = mockResponse();
+    next = mockNext();
+  });
+
+  it('Returns 200 Ok http response when a comment is posted successfully', (done) => {
+    function DummyModel() {
+      return {
+        save: () => Promise.resolve({
+          gif: {
+            title: 'Some title',
+          },
+          comment: {
+            created_on: new Date(),
+            comment: 'Some random comment',
+          },
+        }),
+      };
+    }
+    commentsController = new CommentsController(DummyModel);
+    commentsController.commentOnGif(request, response, next)
+      .then((resp) => {
+        assert.equal(resp.status.args[0][0], 200);
+        done();
+      })
+      .catch((error) => done(error));
+  });
+
+  it('Calls next on when an error is encountered in the execution process', (done) => {
+    function DummyModel() {
+      return {
+        save: () => Promise.reject(),
+      };
+    }
+    commentsController = new CommentsController(DummyModel);
+    commentsController.commentOnGif(request, response, next)
+      .then(() => {
+        assert(next.called);
+        done();
+      })
+      .catch((error) => done(error));
+  });
+});
