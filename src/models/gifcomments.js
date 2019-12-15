32096f1fe03246ea72563cf2f7773a790a577f36
barakaVictor <barakavictor62@gmail.com>
2019-12-15 16:37:16 +0300

    feature(gif comments route): add gif comments route
    add comments route efinition in gif comments routes file
    
    [Finishes]

---
+++ src/models/gifcomments.js
@@ -0,0 +1,33 @@
+const BaseModel = require('./base');
+
+class GifCommentsModel extends BaseModel {
+  constructor(table = 'gifcomments') {
+    super(table);
+  }
+
+  async save(obj) {
+    return this.db.oneOrNone(
+      'SELECT * FROM gifs WHERE id = $1',
+      [obj.gifid],
+    ).then((gif) => {
+      if (!gif) {
+        throw new Error('Related gif does not exist yet');
+      }
+      return this.db.one(
+        'INSERT INTO $1:name($2:name) VALUES($2:list) RETURNING *',
+        [this.table, obj],
+      )
+        .then((comment) => ({
+          comment,
+          gif,
+        }))
+        .catch((error) => {
+          throw new Error(error);
+        });
+    }).catch((error) => {
+      throw new Error(error);
+    });
+  }
+}
+
+module.exports = GifCommentsModel;
--- src/routes/api/v1/gifs/index.js
+++ src/routes/api/v1/gifs/index.js
@@ -1,12 +1,17 @@
 const router = require('express').Router();
 
+const GifCommentsModel = require('../../../../models/gifcomments');
 const GifModel = require('../../../../models/gif');
-
 const GifController = require('../../../../controllers/gifcontroller');
 
+const CommentsController = require('../../../../controllers/commentscontroller');
+
 const gifController = new GifController(GifModel);
+const commentsController = new CommentsController(GifCommentsModel);
+
 
 router.post('/', gifController.upload);
 router.delete('/:gifId', gifController.delete);
+router.post('/:gifId/comments', commentsController.commentOnGif);
 
 module.exports = router;
