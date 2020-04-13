98b6bbdc2dd084f47d1df3fe3aa578829b7a91ee
Victor Baraka <barakavictor62@gmail.com>
2020-04-13 17:46:59 +0300

    chore(tests): improve tests coverage for the project
    add a test suite for the upload utility function

--- src/config/cloudinary-config.js
+++ src/config/cloudinary-config.js
@@ -5,7 +5,7 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
-const { upload } = cloudinary.uploader;
+const cloudinaryUpload = cloudinary.uploader.upload;
 module.exports = {
-  upload,
+  cloudinaryUpload,
 };
--- test/unit/uploaderutility.spec.js
+++ test/unit/uploaderutility.spec.js
@@ -7,12 +7,15 @@ const { mockRequest, mockResponse } = require('../testutils/httpmocks');
 const cloudinary = require('../../src/config/cloudinary-config');
 const multer = require('../../src/config/multer-config');
 
-
+const fs = require('fs')
 const uploader = require('../../src/utils/uploader');
 
 describe('Upload Utility', () => {
   let request;
   let response;
+  let fileSystemUpload;
+  let cloudinaryUpload;
+  let fsOps;
   beforeEach(() => {
     response = mockResponse();
     request = mockRequest({
@@ -20,28 +23,48 @@ describe('Upload Utility', () => {
         path: '',
       },
     });
-    multerUpload = sinon.stub(multer, 'upload').resolves({
-      request: {
-        file: {
-          path: '/some/random/path',
-        },
-      },
-    });
-    cloudinaryUpload = sinon.stub(cloudinary, 'upload').resolves({
+  });
+
+  afterEach(() => {
+    fileSystemUpload.restore();
+    cloudinaryUpload.restore();
+    fsOps.restore();
+  });
+  it('Returns data about an image on successful upload', (done) => {
+    fsOps = sinon.stub(fs, 'unlinkSync')
+    fileSystemUpload = sinon.stub(multer, 'fileSystemUpload').yields()
+    cloudinaryUpload = sinon.stub(cloudinary, 'cloudinaryUpload').yields(null,{
       gifId: 6,
       message: 'GIF image successfully posted',
       createdOn: '2020-04-12T21:47:16.000Z',
       title: 'barney',
       imageUrl: 'https://res.cloudinary.com/baraka/image/upload/v1586738836/iruvktmixjmf0uozueoa.gif',
     });
+    uploader.upload(request, response)
+    .then((result)=>{
+      assert.deepStrictEqual(result,{
+        gifId: 6,
+        message: 'GIF image successfully posted',
+        createdOn: '2020-04-12T21:47:16.000Z',
+        title: 'barney',
+        imageUrl: 'https://res.cloudinary.com/baraka/image/upload/v1586738836/iruvktmixjmf0uozueoa.gif',
+      });
+      done();
+    })
+    .catch((error)=> done(error));
   });
-
-  afterEach(() => {
-    multerUpload.restore();
-    cloudinaryUpload.restore();
-  });
-  it('Returns data about an image on successful upload', (done) => {
-    assert.equal(1 + 1, 2);
-    done();
-  });
+  it('Halts execution and throws an error if local upload fails', (done) => {
+    fsOps = sinon.stub(fs, 'unlinkSync')
+    fileSystemUpload = sinon.stub(multer, 'fileSystemUpload').yields(new Error("An error was encountered"))
+    cloudinaryUpload = sinon.stub(cloudinary, 'cloudinaryUpload').yields(null, {});
+    assert.rejects(uploader.upload(request, response));
+      done();
+    });
+  it('Halts execution and throws an error if cloud upload fails', (done) => {
+    fsOps = sinon.stub(fs, 'unlinkSync')
+    fileSystemUpload = sinon.stub(multer, 'fileSystemUpload').yields()
+    cloudinaryUpload = sinon.stub(cloudinary, 'cloudinaryUpload').yields(new Error("An error was encountered"));
+    assert.rejects(uploader.upload(request, response));
+      done();
+    });
 });
